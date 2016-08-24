a685752f969f19ccb7d2440d91f062c8fe8da900
icefire <stevenaknidev@gmail.com>
2016-08-24 20:06:36 +0300

    Theme stuff

---
+++ wp-content/plugins/ice-theme-settings/ice-theme-settings.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Ice theme settings
+Plugin URI:
+Description: Create custom theme settings
+Version: 0.0.1
+Author: Stevenaknidev@gmail.com
+Author URI: https://ua.linkedin.com/in/steve-arshinikov-5a4184aa
+*/
+
+Class Ice_Theme_Settings {
+    
+}
---
+++ wp-content/themes/ilinqua/page-templates.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: icefier
+ * Date: 24.08.16
+ * Time: 19:42
+ */
+
+/* Template Name: Home Template */
+
+global $core;
+
+$context = $core->get_context();
+$model  = $core->getModel();
+$config = $core->getConfig();
+
