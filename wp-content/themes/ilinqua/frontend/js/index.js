11be868462f26f8057fd63ad4d073a1e2449ca50
Антон Таран <antontaran@brander.ua>
2016-10-02 19:02:06 +0300

    lp portrait

--- wp-content/themes/ilinqua/frontend/js/index.js
+++ wp-content/themes/ilinqua/frontend/js/index.js
@@ -2,10 +2,6 @@ requirejs([
 	'jquery',
 	'ScrollMagic',
 	'animation.gsap',
-	// 'TweenMax',
-	// 'TimelineLite',
-	// 'TimelineMax',
-	// 'TweenLite',
 	'debug.addIndicators'
 
 ], function ($, ScrollMagic) {
@@ -15,22 +11,21 @@ requirejs([
 	var controller = new ScrollMagic.Controller();
 
 	var scene = new ScrollMagic.Scene({
-		// triggerElement: ".price-block",
 		offset: 10,
 		duration: 1000
 
 	})
-	// .setClassToggle(".price-block-holder", "js-del-shadow")
 	.setTween(".price-block-holder", 2, {css:{paddingLeft:"0", paddingRight:"0"}, ease:Power2.easeOut})
 	.addTo(controller);
 
 	new ScrollMagic.Scene({
 		triggerElement: "header",
-		offset: 1000,
+		offset: 900,
 		reverse: true
 	})
-		.setClassToggle(".price-block", "js-del-shadow") // add class toggle
-		// .addIndicators() // add indicators (requires plugin)
-		.addTo(controller);
+
+	.setClassToggle(".price-block", "js-del-shadow") // add class toggle
+	.addTo(controller);
+
 });
 
