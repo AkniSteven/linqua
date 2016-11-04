0d00d8a9c43199255da501b7a7ecce7d1ebdbf27
icefire <stevenaknidev@gmail.com>
2016-11-04 23:08:16 +0200

    course author, author info

--- wp-content/themes/ilinqua/single-course.php
+++ wp-content/themes/ilinqua/single-course.php
@@ -14,4 +14,23 @@ $context = $core->get_context();
 $model  = $core->getModel();
 $config = $core->getConfig();
 
+if ($post) {
+    $model->setResult(['0'=>$post]);
+    $model->formattedACF();
+    $post = $model->getResult();
+    $context['course'] = $post[0];
+}
+if (!empty($context['course'])) {
+    $authorID = $context['course']->post_author;
+    if ($authorID) {
+       $authorData = get_user_by(
+           'ID', $authorID
+       );
+       $authorThumb =   get_field('image', 'user_2')['sizes']['thumbnail'];
+        
+       $context['author_name'] = $authorData ? $authorData->display_name : '';
+       $context['author_thumb'] = $authorThumb ? $authorThumb : '';
+    }
+}
+
 $core->render('single-course.twig', $context);
