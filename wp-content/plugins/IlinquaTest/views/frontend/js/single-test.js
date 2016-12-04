e54f0366cbd9b19c0a0a0acb0c64290ca704b902
Антон Таран <antontaran@brander.ua>
2016-12-04 19:05:32 +0200

    test page

--- wp-content/plugins/IlinquaTest/views/frontend/js/single-test.js
+++ wp-content/plugins/IlinquaTest/views/frontend/js/single-test.js
@@ -1,29 +1,53 @@
 requirejs([
 	'jquery',
+	'masonry.pkgd',
 	'jquery.steps',
 	'jquery.validate',
 
-], function ($) {
+
+], function ($, Masonry) {
 
 	'use strict';
-	console.log('cdscsd');
 
 	var PluginsInit = {
 		init: function () {
 			this.singleTest();
+			this.publicationsGrid();
 		},
 
-
+		/* ========== Tests steps ========== */
 		singleTest: function () {
-			var steps = $(".test-holder");
+			var steps = $(".test-holder").show();
 			steps.steps({
 				headerTag: ".pagination",
 				bodyTag: ".question",
 				transitionEffect: "slideLeft",
 				autoFocus: true,
 				titleTemplate: "#title#",
+				labels: {
+					next: 'Ответ',
+					finish: "Finish",
+				},
 			});
+			$('.answer-button').on('click', function () {
+				steps.steps("next");
+			});
+		},
+		/* ========== END Tests steps  ========== */
+
+		/* ========== Publications grid ========== */
+		publicationsGrid: function () {
+			var elem = document.querySelector('.grid');
+			var msnry = new Masonry( elem, {
+				itemSelector: '.lp-article__tile',
+				columnWidth: 50,
+				rowHeight: 110,
+				percentPosition: true,
+				gutter: 40,
+			});
+
 		}
+		/* ========== END Publications grid  ========== */
 	};
 	PluginsInit.init();
 });
