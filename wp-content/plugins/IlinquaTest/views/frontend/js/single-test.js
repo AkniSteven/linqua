5de8db4d6430ffda359de480222ce3e6867ebde8
Антон Таран <antontaran@brander.ua>
2016-12-11 19:47:56 +0200

    test validation

--- wp-content/plugins/IlinquaTest/views/frontend/js/single-test.js
+++ wp-content/plugins/IlinquaTest/views/frontend/js/single-test.js
@@ -30,9 +30,21 @@ requirejs([
 					finish: "Finish",
 				},
 			});
+
+			/* ========== Check validation inputs ========== */
 			$('.answer-button').on('click', function () {
-				steps.steps("next");
+				var checkRadio = $(this).siblings('.answer-val').find('#radio-some-id1');
+				var checkCheckbox = $(this).siblings('.answer-val').find('#checkbox-some-id1');
+				var answerBlock = $(this).siblings('.answer-val');
+				if (checkRadio.prop('checked') || checkCheckbox.prop('checked')) {
+					console.log('ok');
+					steps.steps("next");
+				} else {
+					console.log('fuck');
+					answerBlock.addClass('error-inputs')
+				}
 			});
+			/* ========== END Check validation inputs  ========== */
 		},
 		/* ========== END Tests steps  ========== */
 
@@ -44,13 +56,30 @@ requirejs([
 				columnWidth: 50,
 				rowHeight: 110,
 				percentPosition: true,
+				transitionDuration: '0.4s',
+				stagger: '0,03s',
 				gutter: 40,
 			});
-
 		}
 		/* ========== END Publications grid  ========== */
 	};
 	PluginsInit.init();
+
+	var singleTestPage = {
+		init: function () {
+			this.showFinishTest();
+		},
+
+		/* ========== Show finish tests page ======= */
+		showFinishTest: function () {
+			var lastBlock = $(".question").last().find('.answer-button');
+			lastBlock.on('click', function () {
+				$('.single-test-page').addClass('js-show-finish-test');
+			});
+		}
+		/* ========== END Show finish page   ========== */
+	};
+	singleTestPage.init();
 });
 
 
