4fd7d3e21c12db7aa7dadf16993a25e9195cd006
icefire <stevenaknidev@gmail.com>
2017-02-08 22:49:56 +0200

    testing js

--- wp-content/plugins/IlinquaTest/views/public/js/testing.js
+++ wp-content/plugins/IlinquaTest/views/public/js/testing.js
@@ -1,25 +1,43 @@
-function addTestStep(data) {
-    debugger;
-        jQuery.ajax({
-            type: 'POST',
-            url: 'http://www.linqua.web/wp-admin/admin-ajax.php',
-            data: {
-                'action': 'addTestStep',
-                'data' : data
-            },
-            dataType: 'html',
-            success: function (response) {
-                alert(response);
+requirejs([
+    'jquery'
+],function($){
+    var testing_obj = {
 
-            },
-            error: function () {
-                alert(0);
-            }
+        submit_btn: '.submit_question',
 
-        })
-}
-jQuery( document ).ready(function() {
-    jQuery('.submit_question').on( "click", function(){
-      addTestStep($(this).closest('form').serialize());
+        init:function(){
+            this.submit_action();
+        },
+
+        submit_action:function(){
+            var _this = this;
+            $(document).on( "click", this.submit_btn, function(){
+                debugger;
+                _this.addTestStep($(this).closest('form').serialize());
+            });
+        },
+
+        addTestStep:function(data) {
+            debugger;
+            $.ajax({
+                type: 'POST',
+                url: 'http://www.linqua.web/wp-admin/admin-ajax.php',
+                data: {
+                    'action': 'addTestStep',
+                    'data': data
+                },
+                dataType: 'html',
+                success: function (response) {
+                    alert(response);
+                },
+                error: function () {
+                    alert(0);
+                }
+
+            })
+        }
+    };
+    $(document).ready(function () {
+        testing_obj.init();
     });
 });
