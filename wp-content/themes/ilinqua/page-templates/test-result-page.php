ee2143651f91d813ee59a17e9b424992e2790a5d
icefire <stevenaknidev@gmail.com>
2017-08-09 23:28:02 +0300

    up test

--- wp-content/themes/ilinqua/page-templates/test-result-page.php
+++ wp-content/themes/ilinqua/page-templates/test-result-page.php
@@ -32,6 +32,34 @@ $currentTestData = $testDb->getTestById($testId)[0];
 if ($currentTestData['test']) {
     $context['name'] = Data::cleanString($currentTestData['name']);
     $context['email'] = Data::cleanString($currentTestData['email']);
+    $context['testID'] = Data::cleanString($currentTestData['testId']);
+    $testAnswers = unserialize($currentTestData['info']);
+
+    if (!empty($testAnswers)) {
+        foreach ($testAnswers as &$answers) {
+            foreach ($answers as &$answer) {
+                if ($answer['id']) {
+                    $question = get_post($answer['id']);
+                    $questionAnswers = get_post_meta($answer['id'], 'answer_case', true);
+                    if (is_array($answer['right_answer'])) {
+                        foreach ($answer['right_answer'] as &$right_answer) {
+                            $right_answer = $questionAnswers[$right_answer];
+                        }
+                    } else {
+                        $answer['right_answer'] = $questionAnswers[$answer['right_answer']];
+                    }
+                    if (is_array($answer['user_answer'])) {
+                        foreach ($answer['user_answer'] as &$user_answer) {
+                            $user_answer = $questionAnswers[$user_answer];
+                        }
+                    } else {
+                        $answer['user_answer'] = $questionAnswers[$answer['user_answer']];
+                    }
+                 }
+            }
+        }
+    }
+    $context['test_answers'] = $testAnswers ? $testAnswers : [];
 }
 
 $core->render('test-result-template.twig', $context);
